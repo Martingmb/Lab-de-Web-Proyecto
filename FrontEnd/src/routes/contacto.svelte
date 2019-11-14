4541c012c49d06f6c042df98b22f5d2f2c04f8c1
Jorge <jorgeolverarmz@gmail.com>
2019-11-13 21:23:34 -0600

    Update contacto.svelte
    Added google maps iframe

--- FrontEnd/src/routes/contacto.svelte
+++ FrontEnd/src/routes/contacto.svelte
@@ -3,18 +3,32 @@
 
 <style>
 
-.formadecontacto {
-  margin: 15px;
-}
+.google-maps {
+        position: relative;
+        padding-bottom: 75%; 
+        height: 0;
+        overflow: hidden;
+    }
+    .google-maps iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100% !important;
+        height: 100% !important;
+    }
 
-.titulocontacto {
-  font-size: 20px;
-  margin-bottom: 15px;
-}
+  .formadecontacto {
+    margin: 15px;
+  }
 
-.notification {
-  background-color: none !important;
-}
+  .titulocontacto {
+    font-size: 20px;
+    margin-bottom: 15px;
+  }
+
+  .notification {
+    background-color: none !important;
+  }
 
 </style>
 
@@ -51,6 +65,11 @@
 
 </div>
 </div>
+
+<div class ="google-maps">
+        <iframe src= "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28815.797537515813!2d-100.20473231599998!3d25.472520511191355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8662c8c0c7fdb341%3A0x22eddf316bb17a4e!2sEl%20Barrial%2C%2067300%20Santiago%2C%20Nuevo%20Leon!5e0!3m2!1sen!2smx!4v1573689974585!5m2!1sen!2smx" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+</div>
+ 
 
     
     
