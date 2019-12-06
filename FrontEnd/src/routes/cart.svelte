a4ea90d1faabc67bd97627fac0435808dcea52c7
Andrés Sánchez <andres_sa.97@hotmail.com>
2019-12-06 03:14:21 -0600

    Cart page work

--- FrontEnd/src/routes/cart.svelte
+++ FrontEnd/src/routes/cart.svelte
@@ -1,4 +1,5 @@
 <script>
+	import * as sapper from '@sapper/app';
 	import { onMount } from 'svelte';
 	let cart = []
 	onMount(()=>{
@@ -10,9 +11,43 @@
 	function addCommas(x) {
 		return x.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	}
+
+	function checkout(){
+		const url = 'http://localhost:2020/checkout';
+		fetch(url, {
+			method: 'POST',
+			body: JSON.stringify({ items: cart }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).then(response=>{
+			response.json().then(res=>{
+				console.log(res);
+				if(res.error){
+					alert(res.error.message);
+					return;
+				}
+				localStorage.setItem('order', JSON.stringify(res.data));
+				localStorage.removeItem('cart');
+				sapper.goto('order/checkout');
+			})
+		}).catch(err=>{
+			alert("Error haciendo login.");
+		})
+	}
 </script>
+<style>
+	.container{
+		max-width: 700px;
+	}
+	.container .notification{
+		margin-bottom: 10px;
+	}
+	.btn{
+		cursor: pointer;
+	}
+</style>
 <nav class="navbar navbar-expand-lg navbar-light bg-light" id ="navContainer3" style="margin-bottom: 10px;">
-	<a class="navbar-brand" href="#"></a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
@@ -32,7 +67,7 @@
 </nav>
 	
 <div class="container">
-	<h1 class="title">CART</h1>
+	<h1 class="title">Carrito</h1>
 	<div class="notification">
 		<table class="table">
 			<thead>
@@ -44,6 +79,11 @@
 				</tr>
 			</thead>
 			<tbody>
+				{#if cart.length==0}
+				<tr>
+					<td colspan="4" style="text-align: center; font-weight: 600; font-size: 24px;">Carrito vacio</td>
+				</tr>
+				{/if}
 				{#each cart as item}
 				<tr>
 					<td>{item.name}</td>
@@ -52,7 +92,14 @@
 					<td>${addCommas(item.cost*item.quantity)}</td>
 				</tr>
 				{/each}
+				<tr>
+					<td colspan="3" style="font-weight: 600; text-align: right;">Total</td>
+					<td>${ addCommas(cart.reduce((a,b)=>a+(b.cost*b.quantity), 0)) }</td>
+				</tr>
 			</tbody>
 		</table>
+	</div>
+	<div style="text-align: right; margin-top: 0">
+		<div class="btn btn-primary" style="color:#ffffff;" on:click={checkout}>Checkout</div>
 	</div>
 </div>
